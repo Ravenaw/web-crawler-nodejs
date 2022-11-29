04e9af5a9c8546a6bb28a833757e2482e2d946c9
Ravenaw <58209261+Ravenaw@users.noreply.github.com>
2022-11-29 14:13:12 +0100

    -

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function main(maxPages = 50) {
+//Product schema
+class Product {
+    constructor(product_name, product_description, main_category, sub_category, price, link, image_url) {
+        this.product_name = product_name;
+        this.product_description = product_description;
+        this.main_category = main_category;
+        this.sub_category = sub_category;
+        this.price = price;
+        this.link = link;
+        this.image_url = image_url;
+    }
+}
+
+async function main(maxPages = 10) {
     // initialized with the first webpage to visit
-    const paginationURLsToVisit = ["https://scrapeme.live/shop"];
+    const mainURL = "https://www.proshop.dk";
+    const paginationURLsToVisit = ["https://www.proshop.dk/baerbar", "https://www.proshop.dk/mobil"];
     const visitedURLs = [];
 
     const productURLs = new Set();
 
+    const products = [];
+
     // iterating until the queue is empty
     // or the iteration limit is hit
     while (
-        paginationURLsToVisit.length !== 0 &&
-        visitedURLs.length <= maxPages
-        ) {
+        paginationURLsToVisit.length !== 0
+    ) {
         // the current webpage to crawl
         const paginationURL = paginationURLsToVisit.pop();
 
@@ -28,8 +43,8 @@ async function main(maxPages = 50) {
         const $ = cheerio.load(pageHTML.data);
 
         // retrieving the pagination URLs
-        $(".page-numbers a").each((index, element) => {
-            const paginationURL = $(element).attr("href");
+        $(".pagination a").each((index, element) => {
+            const paginationURL = mainURL + $(element).attr("href");
 
             // adding the pagination URL to the queue
             // of web pages to crawl, if it wasn't yet crawled
@@ -42,16 +57,31 @@ async function main(maxPages = 50) {
         });
 
         // retrieving the product URLs
-        $("li.product a.woocommerce-LoopProduct-link").each((index, element) => {
-            const productURL = $(element).attr("href");
+        $("li.toggle a.site-product-link").each((index, element) => {
+            const productURL = mainURL + $(element).attr("href");
             productURLs.add(productURL);
         });
     }
 
     // logging the crawling results
-    console.log([...productURLs]);
+    //console.log([...productURLs]);
 
     // use productURLs for scraping purposes...
+    productURLs.forEach(async (productURL) => {
+        const pageHTML = await axios.get(productURL);
+        const $ = cheerio.load(pageHTML.data);
+        const product= new Product({
+            product_name: $(".hidden-xs li").text(),
+            product_description: $(".site-product-short-description").text(),
+            main_category: $(".breadcrumb li:nth-child(2)").text(),
+            sub_category: $(".breadcrumb li:nth-child(3)").text(),
+            price: $(".site-currency-attention").text(),
+            link: productURL,
+            image_url: $(".h-auto").attr("src")
+        });
+        console.log($(".site-stock-text div").text());
+        products.push(product);
+    });
 }
 
 main()
